56cc7538d2d8624aedd181b32c6a308760db7183
Nick Banford <nickbanford@gmail.com>
2014-11-30 12:46:16 +0000

    Displays specified display text
    Displays specified display text for the selected items and removes it
    when they're unselected.

--- src/cg-directory.js
+++ src/cg-directory.js
@@ -13,7 +13,18 @@ var cg = {};
         // Private
         var input = $(settings.inputSelector),
             container = $('<div class="cg-directory-dialog"></div>'),
+            displayList = $("<ul></ul>"),
             items = [],
+            
+            checkboxClass = "cg-directory-checkbox",
+            
+            buildTableRow = function(data){
+                var checkbox = '<input class="' + checkboxClass + '" type="checkbox" value="' 
+                    + data.id + '" data-display="' + data.surname + '" />';
+                var checkboxCell = '<td>' + checkbox + '</td>';
+                var dataCell = "<td>" + data.surname + "</tr>"
+                return '<tr class="cd-directory-item">' + checkboxCell + dataCell + "</td>";
+            },
             
             buildTable = function(items){
                 var table = $("<table></table>");
@@ -23,12 +34,9 @@ var cg = {};
                 var tableBody = $("<tbody></tbody>");
                 table.append(tableBody);
                 
-                for(var i=0; i<items.length; i++){
-                    var checkbox = '<input class="cg-directory-checkbox" type="checkbox" value="' + items[i].id + '" />';
-                    var checkboxCell = '<td>' + checkbox + '</td>';
-                    var dataCell = "<td>" + items[i].surname + "</tr>"
-                    
-                    tableBody.append('<tr class="cd-directory-item">' + checkboxCell + dataCell + "</td>");   
+                for(var i=0; i<items.length; i++){          
+                    var row = buildTableRow(items[i]);
+                    tableBody.append(row);   
                 }
             },
             
@@ -43,6 +51,8 @@ var cg = {};
                     var checkboxes = $('.cg-directory-checkbox');
                     
                     checkboxes.click(toggleItem);
+                    
+                    console.log(selectedItems);
                 });
             },
             
@@ -51,36 +61,53 @@ var cg = {};
                 input.val(ids);
             },
             
+            updateDisplay = function(){
+                displayList.html("");
+                
+                for(var i=0; i < selectedItemDisplay.length; i++){
+                    displayList.append("<li>" + selectedItemDisplay[i] + "</li>");   
+                }
+            },
+            
             toggleItem = function(item){
                 var isChecked = item.target.checked;
                 var value = item.target.value;
+                var displayText = item.target.getAttribute("data-display");
                 
                 if(isChecked){   
                     selectedItems.push(value);
+                    selectedItemDisplay.push(displayText);
                 } else {
                     var index = selectedItems.indexOf(value);
                     selectedItems.splice(index, 1);
+                    
+                    var displayIndex = selectedItemDisplay.indexOf(displayText);
+                    selectedItemDisplay.splice(displayIndex, 1);
                 }
                 
                 updateInput();
+                updateDisplay();
             };
         
         var selectedItems = [], 
         
             init = function(){
-            var trigger = '<a href="#" class="cg-trigger">' + settings.triggerText  + '</a>';
-            input.after(trigger);
+                var trigger = $('<a href="#" class="cg-trigger">' + settings.triggerText  + '</a>');
+                input.after(trigger);
+                trigger.after(displayList);
             
-            $(".cg-trigger").click(show);
-        },
+                $(".cg-trigger").click(show);
+            },
             
-        show = function(){
-            loadData();
+            selectedItemDisplay = [],
             
-            container.dialog({
-                title: 'Directory'   
-            });
-        };
+            show = function(){
+                loadData();
+                
+                container.dialog({
+                    title: 'Directory'   
+                });
+            };
         
         // Public API
         return {
